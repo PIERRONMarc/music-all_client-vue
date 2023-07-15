0651674e4dc3616a3f98981ed91a6ff247ef753a
PIERRON Marc <pierronmarc@outlook.fr>
2023-07-16 01:32:49 +0200

    chore: install Pinia package

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,15 @@ import { worker } from "@/mocks/browser";
 import './assets/main.css'
 import "@fontsource/roboto"
 import "@fontsource/montserrat"
+import {createPinia} from "pinia";
 
 if (import.meta.env.DEV) {
     worker.start()
 }
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(router)
 app.mount('#app')
