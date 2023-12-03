28207698a1d7d3315b3260738aa4b84a42a796cf
PIERRON Marc <pierronmarc@outlook.fr>
2023-12-03 21:22:24 +0100

    chore(AppModal): create AppModal component

---
+++ src/components/AppModal/AppModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <Teleport to="body">
+    <div
+        class="left-0 top-0 z-50 fixed w-full h-full grid place-items-center bg-black/20 px-6"
+        @click.self="$emit('update:modelValue', false)"
+        v-if="modelValue"
+    >
+        <slot></slot>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: boolean;
+}
+
+interface Emits {
+  (event: "update:modelValue", value: boolean): void;
+}
+
+defineProps<Props>();
+defineEmits<Emits>();
+</script>
