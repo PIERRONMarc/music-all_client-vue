3fb15c9dfb51fce565655e4f7196a90d4faf8c01
PIERRON Marc <pierronmarc@outlook.fr>
2023-12-10 21:09:45 +0100

    chore(HomePage): update Hero

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -8,15 +8,16 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-row">
+  <div class="flex flex-row justify-between">
     <div>
       <h1 class="font-montserrat text-6xl md:text-8xl font-normal m-0 mb-5 text-white">Music'<span class="text-primary-color">all</span></h1>
-      <p class="text-base m-0 mb-10 w-full md:w-3/4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra ut purus et blandit. Proin sit amet justo vulputate, dictum enim non, finibus lacus.
+      <p class="text-base m-0 mb-10 w-full">
+        Listen to your favorite music with your friends ! <br>
+        Made with &#10084;&#65039; by <a class="text-primary-color italic" href="https://marcpierron.dev/">@MarcPierron</a>
       </p>
       <Button :on-click="onCreateRoom">Create my room</Button>
     </div>
-    <OnlineMedia class="hidden min-[992px]:block -right-24 relative" />
+    <OnlineMedia class="hidden min-[992px]:block -right-24 relative w-60 h-60" />
   </div>
 </template>
 
--- src/pages/HomePage/HomeTemplate/HomeTemplate.vue
+++ src/pages/HomePage/HomeTemplate/HomeTemplate.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex flex-col md:flex-row min-h-screen relative">
     <div class="w-full md:w-3/4 h-auto md:h-screen bg-bg-color flex flex-col md:flex-row justify-center items-center relative z-10 bg-treble-clef-background bg-no-repeat bg-40%">
-      <div class="p-8 p-16 md:pl-28">
+      <div class="p-16 md:pl-28 w-full">
         <slot name="hero"></slot>
       </div>
       <div id="mello" class="static md:absolute z-50 bottom-0 left-[20%]">
